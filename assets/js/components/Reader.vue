7105b9a719caa16ca10ebd0992b23e4c4f7b44bf
shokohsc <shokohsc@gmail.com>
2019-05-12 16:47:23 +0200

    added reader component

---
+++ assets/js/components/Reader.vue
@@ -0,0 +1,81 @@
+<template>
+    <div :style="{ height: height + 'px' }">
+        <div v-on:click="previousPage" class="left"></div>
+        <div v-on:click="close" class="center"></div>
+        <div v-on:click="nextPage" class="right"></div>
+        <img :src="image" :width="width" class="image"/>
+    </div>
+</template>
+
+<script>
+    export default {
+        computed: {
+            index: function() {
+                return this.$store.getters['comic/index'];
+            },
+            image: function() {
+                return 'data:' + this.type + ';base64,' + this.$store.getters['comic/page']['image'];
+            },
+            width: function() {
+                return window.screen.availWidth;
+            },
+            height: function() {
+                let height = this.$store.getters['comic/page']['height'] * this.width / this.$store.getters['comic/page']['width'];
+                return Math.floor(height);
+            },
+            type: function() {
+                return this.$store.getters['comic/page']['type'];
+            },
+        },
+        methods: {
+            close: function() {
+                this.$eventBus.$emit('close-comic');
+            },
+            nextPage: function() {
+                this.$store.commit('comic/increaseIndex');
+            },
+            previousPage: function() {
+                this.$store.commit('comic/decreaseIndex');
+            }
+        }
+    };
+</script>
+
+<style>
+.left {
+    position: absolute;
+    left: 0;
+    z-index: 1;
+    height: inherit;
+    width: 40%;
+    display: inline-block;
+    /* opacity: 0.5;
+    background-color: #F00; */
+}
+.center {
+    position: absolute;
+    left: 40%;
+    z-index: 1;
+    height: inherit;
+    width: 20%;
+    display: inline-block;
+    /* opacity: 0.5;
+    background-color: #0F0; */
+}
+.right {
+    position: absolute;
+    left: 60%;
+    z-index: 1;
+    height: inherit;
+    width: 40%;
+    display: inline-block;
+    /* opacity: 0.5;
+    background-color: #00F; */
+}
+.image {
+    position: absolute;
+    left: 0;
+    z-index: -1;
+    width: 100%;
+}
+</style>
